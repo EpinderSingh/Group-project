69cc21fa8c17838dcc54b627c804f463501d2a79
Laura <lauraduggan89@gmail.com>
2020-05-07 11:57:46 +0800

    * appsettings.json:
    * ContactApiController.cs: contactAPI working - may need to be revised

--- CoWork454/Controllers/Api/ContactApiController.cs
+++ CoWork454/Controllers/Api/ContactApiController.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 using CoWork454.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using SendGrid;
-using SendGrid.Helpers.Mail;
+using System.Net.Mail;
+using System.Net;
 
 namespace CoWork454.Controllers.Api
 {
@@ -18,21 +18,33 @@ namespace CoWork454.Controllers.Api
         [HttpPost]
         public void Post([FromBody] ContactForm model)
         {
-            Execute(model).Wait();
+            var fromAddress = new MailAddress("cowork454team2@gmail.com", "CoWork454");
+            var toAddress = new MailAddress("cowork454team2@gmail.com", "CoWork454");
+            const string fromPassword = "&u},IRX<A+hv8Tsw";
+            var subject = $"New Contact from Contact Form: {model.SubjectLine}";
+            var body = $"New message from: {model.FirstName} {model.LastName}" +
+                $"<br /> Email address: {model.EmailAddress}" +
+                $"<br /> Phone number: {model.PhoneNumber}" +
+                $"<br /> Message: {model.MessageBody}";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+                Timeout = 20000
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
         }
 
-        static async Task Execute(ContactForm model)
-        {
-            //need to move API key out of file ****
-            var apiKey = "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";
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("test@cowork454.com", "CoWork454 Superuser");
-            var subject = model.SubjectLine;
-            var to = new EmailAddress("lauraduggan89@gmail.com", "Laura Duggan");
-            var plainTextContent = model.MessageBody;
-            var htmlContent = model.MessageBody;
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
-        }
     }
 }
