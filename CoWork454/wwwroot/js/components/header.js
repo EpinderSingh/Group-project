fd3aca8b4a389726577c80fe228b1e15387ddc55
Epinder <epinder.singh@gmail.com>
2020-05-12 07:16:06 +0800

    minor changes to side sliding navbar

--- CoWork454/wwwroot/js/components/header.js
+++ CoWork454/wwwroot/js/components/header.js
@@ -1,10 +1,26 @@
-﻿using System;
-namespace CoWork454.wwwroot.js.components
-{
-	public class header
-	{
-		public header()
-		{
-		}
-	}
-}
+﻿const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', (e) => {
+        e.preventDefault();
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${
+                    index / 7 + 0.3
+                    }s`;
+            }
+        });
+        //Bruger Animation
+        burger.classList.toggle('toggle');
+    });
+};
+
+navSlide();
