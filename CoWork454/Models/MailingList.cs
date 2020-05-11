1ba55b453d751a887ac5a76da0fe33dedb417ed4
Edward <ej.speight@gmail.com>
2020-05-11 11:08:18 +0800

    Adding Mailing List Model

---
+++ CoWork454/Models/MailingList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CoWork454.Models
+{
+    public class MailingList
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
