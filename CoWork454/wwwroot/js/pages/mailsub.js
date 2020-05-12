36f7fc65510b47bcc18afba7b9bf294b37dd470f
Edward <ej.speight@gmail.com>
2020-05-12 10:18:53 +0800

    whip

---
+++ CoWork454/wwwroot/js/pages/mailsub.js
@@ -0,0 +1,28 @@
+﻿let mailFirstName = document.querySelector('#mailFirstName');
+let mailLastName = document.querySelector('#mailLastName');
+let subEmail = document.querySelector('#subEmail');
+let subscriptionForm = document.querySelector('.mailingList');
+subscriptionForm.addEventListener('submit', (event) => {
+    alert('yo')
+    event.preventDefault();
+    const body = {
+        firstName: mailFirstName.value,
+        lastName: mailLastName.value,
+        emailAddress: subEmail.value,
+    }
+    const sendMessage = {
+        body: JSON.stringify(body),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    fetch('https://localhost:5001/api/MailingListApi', sendMessage)
+        .then(res => res.json())
+        .then(() => {
+            alert("Thank you for your subscription");
+        })
+        .catch(err => {
+        });
+
+});
