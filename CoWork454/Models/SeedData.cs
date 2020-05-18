d4c19d3d09f5ca76d712a022a0602eb56a894483
Laura <lauraduggan89@gmail.com>
2020-05-18 16:48:42 +0800

    added seed data to DB

---
+++ CoWork454/Models/SeedData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using MvcMailingList.Data;
+
+namespace CoWork454.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMailingListContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMailingListContext>>()))
+            {
+                // Look for any super user logins.
+                if (context.Users.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Users.AddRange(
+                    new User
+                    {
+                        FirstName = "CoWork454",
+                        LastName = "Admin",
+                        EmailAddress = "admin@cowork454.com",
+                        PasswordHash = "$2a$11$JrFlOUbUXB5MdhriMuCqye3XNolKNbxX2JeJBai71Vs47WsgT/nq6",
+                        IsAdmin = true
+
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
