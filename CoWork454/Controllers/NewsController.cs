81699da40f392d68ae41588c07b0e7e65f2d7391
Epinder <epinder.singh@gmail.com>
2020-05-18 14:51:51 +0800

    news index page

---
+++ CoWork454/Controllers/NewsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcMailingList.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoWork454.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly MvcMailingListContext _context;
+
+        public NewsController(MvcMailingListContext mvcMailingList)
+
+        {
+            _context = mvcMailingList;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewData["news"] = _context.NewsPosts.OrderByDescending(p => p.DateTimePosted).ToList();
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        
+    }
+}
--- CoWork454/wwwroot/js/pages/news.js
+++
@@ -1,7 +0,0 @@
-﻿fetch('/api/NewsPostApi')
-    .then(res => res.json())
-    .then((data) => {
-        console.log(data)
-    })
-    .catch(err => {
-    });
